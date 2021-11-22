8ea1a5f3fd51f7a2a3fe61de1e204f3dd95af253
Christian Bergschneider <37768199+Bloeckchengrafik@users.noreply.github.com>
2021-11-22 15:59:23 +0100

    MC-101 sign done

--- src/main/java/net/galaxycore/citybuild/commands/SignCommand.java
+++ src/main/java/net/galaxycore/citybuild/commands/SignCommand.java
@@ -11,11 +11,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 public class SignCommand implements CommandExecutor {
     private final HashMap<Player, Long> signCooldown = new HashMap<>();
@@ -43,13 +42,30 @@ public class SignCommand implements CommandExecutor {
                 }
             }
         }
+
+        String text = "";
+
+        if (args.length > 0) {
+            text = String.join(" ", args);
+        }
+
         signCooldown.put(player, System.currentTimeMillis() + 86400000); // 86400000ms = 24H
         ItemStack item = player.getInventory().getItemInMainHand();
         ItemMeta itemMeta = item.getItemMeta();
-        itemMeta.lore(Collections.singletonList(Component.text(StringUtils.replaceRelevant(I18N.getByPlayer(player, "citybuild.sign.text"), new LuckPermsApiWrapper(player)).replaceAll("%time%", new SimpleDateFormat(
+        ArrayList<Component> lore = new ArrayList<>();
+
+        if (itemMeta.lore() != null)
+            lore.addAll(itemMeta.lore());
+
+        if(!text.equals(""))
+            lore.add(Component.text("§7" + text));
+
+        lore.add(Component.text(StringUtils.replaceRelevant(I18N.getByPlayer(player, "citybuild.sign.text"), new LuckPermsApiWrapper(player)).replaceAll("%time%", new SimpleDateFormat(
                 I18N.getInstanceRef().get().getLanguages()
                         .get(I18N.getInstanceRef().get().getLocale(player)).getDateFormat())
-                .format(new Date(System.currentTimeMillis()))))));
+                .format(new Date(System.currentTimeMillis())))));
+
+        itemMeta.lore(lore);
         item.setItemMeta(itemMeta);
         player.sendMessage(I18N.getByPlayer(player, "citybuild.sign.success"));
         return true;

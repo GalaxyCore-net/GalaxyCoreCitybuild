f6805018e3149ea362ea994ea2d9147e408c343f
Christian Bergschneider <37768199+Bloeckchengrafik@users.noreply.github.com>
2022-03-03 20:05:55 +0100

    MC-101 Fix some mistakes with the block list menu

--- src/main/java/net/galaxycore/citybuild/pmenu/menu/flags/BlockListMenu.java
+++ src/main/java/net/galaxycore/citybuild/pmenu/menu/flags/BlockListMenu.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlockListMenu extends Menu {
-    public static final int PIVOT = 5*9;
+    public static final int PIVOT = 5*9+1;
     private final Player player;
     private final Plot plot;
     private final Flag flag;
@@ -28,9 +28,11 @@ public class BlockListMenu extends Menu {
         this.plot = plot;
         this.flag = flag;
         this.blockListFlagMenu = blockListFlagMenu;
+        itemStacks.clear();
 
         for (Material value : Material.values()) {
             if (!value.isBlock()) continue;
+            if (value.isAir()) continue;
             itemStacks.add(new ItemStack(value, 1));
         }
     }
@@ -47,18 +49,49 @@ public class BlockListMenu extends Menu {
 
     @Override
     public void handleMenu(InventoryClickEvent inventoryClickEvent) {
+        int raw = inventoryClickEvent.getRawSlot();
 
+        if(raw == 5*9) {
+            blockListFlagMenu.open();
+        } else if(raw == 5*9+3){
+            page--;
+            if(page < 0)
+                page = 0;
+            open();
+        } else if(raw == 5*9+5) {
+            page++;
+            open();
+        }
+
+        if (raw >= 5*9) {
+            return;
+        }
+
+        ItemStack item = inventoryClickEvent.getCurrentItem();
+        if(item == null) return;
+
+        System.out.println(item.getType());
     }
 
     @Override
     public void setMenuItems() {
         int len = itemStacks.size();
-        page = (int) Math.min(page, Math.ceil(len/(float)PIVOT));
         int start = page * PIVOT;
-        int end = Math.min((page*PIVOT)-1, len-(page*PIVOT));
+        int end = Math.min(((page+1)*PIVOT)-1, len-(page*PIVOT));
+        while(start > end) {
+            page--;
+            start = page * PIVOT;
+            end = Math.min(((page+1)*PIVOT)-1, len-(page*PIVOT));
+        }
 
         for (int i = start; i < end; i++) {
             inventory.addItem(itemStacks.get(i));
         }
+
+        inventory.setItem(5*9, makeItem(Material.BARRIER, PMenuI18N.BACK.get(player)));
+        inventory.setItem(5*9+3, makeItem(Material.ARROW, PMenuI18N.PREV.get(player)));
+        inventory.setItem(5*9+4, makeItem(Material.PAPER, PMenuI18N.PAGE.get(player) + (page + 1)));
+        inventory.setItem(5*9+5, makeItem(Material.ARROW, PMenuI18N.NEXT.get(player)));
+        setFillerGlass();
     }
 }
